6dae1716ff2ad6d32b218ed0e5270c6ec8f6fe66
Cecilia Aguayo <78163790+CAguayoRb@users.noreply.github.com>
2021-06-16 17:46:56 -0700

    did_it_after_all

---
+++ PyBank/main.py
@@ -0,0 +1,62 @@
+import csv
+import os
+
+budgetdata_csv = os.path.join("python-challenge/PyBank/Resources/budget_data.csv")
+
+with open(budgetdata_csv, newline = "") as data_csv:
+    csvreader = csv.reader(data_csv, delimiter=',')
+    header = next(csvreader)
+
+    months_var = 0 
+    profit_loss_total = []
+    total_months = []
+    change_months = []
+
+    #get total number of monts, total profit & loss, and data.
+    for row in csvreader:
+        total_months.append(row[0])
+        profit_loss_total.append(int(row[1]))
+ 
+
+    for i in range(len(profit_loss_total)-1):
+        change_months.append(profit_loss_total[i+1]-profit_loss_total[i])
+    
+    total_months = len(total_months) 
+    total_sum = sum(profit_loss_total)
+    average_change = round(sum(change_months)/len(change_months), 2)
+    greatest_increase = max(change_months)
+    greatest_decrease = min(change_months)
+
+    print('Financial Analysis;')
+    print('-------------------------------------')
+    print(f'Total Months: {total_months}')
+    print(f'Total: ${total_sum}')
+    print(f'Average Change: ${average_change}')
+    print(f'Greatest Increase: ${greatest_increase}')
+    print(f'Greatest Decrease: ${greatest_decrease}')
+   
+
+#outputfile = 'Analysis/budget_data_hl.txt'
+
+import sys
+sys.stdout = open("python-challenge/PyBank/Analysis/budget_data_analysis.txt", "w")
+
+print("Financial Analysis;")
+print("-------------------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${total_sum}")
+print(f"Average Change: ${average_change}")
+print(f"Greatest Increase: ${greatest_increase}")
+print(f"Greatest Decrease: ${greatest_decrease}")
+   
+sys.stdout.close()
+
+#with open(outputfile, 'w') as output:
+#    output.write('Financial Analysis\n')
+#    output.write('--------------------------------\n')
+ #   output.write(f'Total Months: {total_months}\n')
+ #   output.write(f'Total: ${(total_change))}\n')
+ #   output.write(f'Average Change: ${change_months}\n')
+  #  output.write(f'Greatest Increase: ${greatest_increase}\n')
+  #  output.write(f'Greatest Decrease: ${greatest_decrease}\n')
+
---
+++ PyPoll/main.py
@@ -0,0 +1,79 @@
+import os
+import csv
+from collections import Counter
+import sys
+
+poll_csv = 'python-challenge/PyPoll/Resources/election_data.csv'
+
+with open(poll_csv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(csvreader)
+
+    totalvotes = 0
+    votes = []
+    candidates = []
+   
+    for row in csvreader:
+        votes.append(row[0])
+        candidates.append(row[2])
+
+    total_votes = (len(votes))
+
+#Get info for each candidate:
+
+#Otooley
+otooley_votes = (int(candidates.count("O'Tooley")))
+otooley_percentage = (otooley_votes/total_votes) * 100
+otooley_final = round(otooley_percentage, 3)
+
+#Correy
+correy_votes = (int(candidates.count("Correy")))
+correy_percentage = (correy_votes/total_votes) * 100
+correy_final = round(correy_percentage, 3)
+
+#Li
+li_votes = (int(candidates.count("Li")))
+li_percentage = (li_votes/total_votes) * 100
+li_final = round(li_percentage, 3)
+
+#Khan
+khan_votes = (int(candidates.count("Khan")))
+khan_percentage = (khan_votes/total_votes) * 100
+khan_final = round(khan_percentage, 3)
+
+#Get the winner:
+if otooley_votes > khan_votes > correy_votes > li_votes:
+    winner = "O'Tooley"
+elif correy_votes > khan_votes > li_votes > otooley_votes:
+    winner = "Correy"
+elif li_votes > khan_votes > correy_votes > otooley_votes:
+    winner = "Li"
+elif khan_votes > correy_votes > li_votes > otooley_votes:
+    winner = "Khan"
+
+
+print("Election Results")
+print("----------------------------------------")
+print(f"Total Votes: {total_votes}")
+print("----------------------------------------")
+print(f"Khan: {khan_percentage}% ({khan_votes})")
+print(f"Correy: {correy_percentage}% ({correy_votes})")
+print(f"Li: {li_percentage}% ({li_votes})")
+print(f"O'Tooley: {otooley_percentage}% ({otooley_votes})")
+print("-------------------------------------------")
+print(f"Winner: {winner}!")
+
+sys.stdout = open("python-challenge/PyPoll/Analysis/election_data_analysis.txt", "w")
+
+print("Election Results")
+print("----------------------------------------")
+print(f"Total Votes: {total_votes}")
+print("----------------------------------------")
+print(f"Khan: {khan_percentage}% ({khan_votes})")
+print(f"Correy: {correy_percentage}% ({correy_votes})")
+print(f"Li: {li_percentage}% ({li_votes})")
+print(f"O'Tooley: {otooley_percentage}% ({otooley_votes})")
+print("-------------------------------------------")
+print(f"Winner: {winner}!")
+
+sys.stdout.close()
